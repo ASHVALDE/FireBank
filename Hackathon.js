c583eecb4416fd7e01a4bc195eb51dfa1ff3ec3f
ASH (Santiago Valderrama) <69223535+ASHVALDE@users.noreply.github.com>
2021-11-19 06:39:26 -0500

    puerto cambiado

--- Hackathon.js
+++ Hackathon.js
@@ -2,7 +2,7 @@ const express = require('express')
 const path = require('path');
 const session = require("express-session")
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 let options = {};
 var request = require('request');
 
